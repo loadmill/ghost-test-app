6195178461b947c390a644b7cb5c6dfb77dcccbb
Ido Cohen <ido.c.mail@gmail.com>
2019-05-30 15:32:16 +0300

    inject loadmill recorder

--- inject.js
+++ inject.js
@@ -1,16 +1,15 @@
 
- window.loadmillUserId = "21"
- 
- if ('serviceWorker' in navigator) {
-    var swUrl = `worker.js?loadmillUserId=${window.loadmillUserId}&notSecure=true`;
-    window.addEventListener('load', function() {
-        navigator.serviceWorker.register(swUrl).then(function(registration) {
-            // Registration was successful
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }, function(err) {
-            // registration failed :/
-            console.log('ServiceWorker registration failed: ', err);
-            });
-        });
-}
+window.loadmill_code = "c28400f5-2205-4a52-9a68-515bc3887491";
 
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        const url = `worker.js?loadmill_code=${window.loadmill_code}&notSecure=true`
+        navigator.serviceWorker.register(url).then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+        // registration failed :/
+        console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
