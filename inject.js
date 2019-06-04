607c0987dcab66d3950be65d832199f25d8b436c
Ido Cohen <ido.c.mail@gmail.com>
2019-06-04 17:28:49 +0300

    Update inject.js

--- inject.js
+++ inject.js
@@ -1,5 +1,5 @@
 
-window.loadmill_code = "c28400f5-2205-4a52-9a68-515bc3887491";
+window.loadmill_code = "3e6a533e-b84e-4879-88e0-4409a5a71eb7";
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
