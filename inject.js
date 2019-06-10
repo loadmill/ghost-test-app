939a31f43f3c64b12d892823b5a503ca159b47a6
Ido Cohen <ido.c.mail@gmail.com>
2019-06-10 14:40:51 +0300

    Update inject.js

--- inject.js
+++ inject.js
@@ -3,7 +3,7 @@ window.loadmill_code = "e7cdcd0b-d410-4404-bb7c-72f07ed60274";
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-        const url = `worker.js?loadmill_code=${window.loadmill_code}&notSecure=true`
+        const url = `loadmill-worker.js?loadmill_code=${window.loadmill_code}&notSecure=true`
         navigator.serviceWorker.register(url).then(function(registration) {
         // Registration was successful
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
