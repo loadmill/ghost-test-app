7a071ce54172ba8adadd98954798025fccfd50df
Ido Cohen <ido.c.mail@gmail.com>
2019-06-10 12:09:04 +0300

    Update add_worker.sh

--- add_worker.sh
+++ add_worker.sh
@@ -1,4 +1,4 @@
 
 
 sed -e '/The main JavaScript file for Casper/a \
-<script src="./inject.js"> </script>' ./content/themes/casper/default.hbs > ./content/themes/casper/tmp-file && mv ./content/themes/casper/tmp-file ./content/themes/casper/default.hbs
+<script src="/inject.js"> </script>' ./content/themes/casper/default.hbs > ./content/themes/casper/tmp-file && mv ./content/themes/casper/tmp-file ./content/themes/casper/default.hbs
