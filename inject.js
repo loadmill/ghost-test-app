bedd23fcf0d3d5fc02691e6f6f608353cec218c1
Ido Cohen <ido.c.mail@gmail.com>
2019-06-28 17:11:42 +0300

    New inject script with postMessage

--- inject.js
+++ inject.js
@@ -1,15 +1,11 @@
 
 window.loadmill_code = "e7cdcd0b-d410-4404-bb7c-72f07ed60274";
-
 if ('serviceWorker' in navigator) {
+    var lm_w = navigator.serviceWorker;
     window.addEventListener('load', function() {
-        const url = `loadmill-worker.js?loadmill_code=${window.loadmill_code}&not_secure=true`
-        navigator.serviceWorker.register(url).then(function(registration) {
-        // Registration was successful
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-        // registration failed :/
-        console.log('ServiceWorker registration failed: ', err);
+        const url = `loadmill-worker.js?loadmill_code=${window.loadmill_code}&not_secure=true`;
+        lm_w.register(url).then(function () {
+            lm_w.controller && lm_w.controller.postMessage({ type: "page-loaded" });
         });
     });
 }
